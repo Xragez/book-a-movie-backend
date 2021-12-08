e61b6bd71225959012da147f6f5c544c1eae733a
Pawel Zegarski <pawelzeg@gmail.com>
2021-12-08 01:23:59 +0100

    Add gitignore and database configuration

---
+++ src/main/java/com/bookamovie/be/BookAMovieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookamovie.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAMovieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAMovieBackendApplication.class, args);
+	}
+
+}
